7d6a14e8f2f48ea8c7901aef1a23e69e638f1b46
Antoine Niek <antoine.niek@adgear.com>
2018-04-03 18:43:30 -0400

    Version the build for direct github consumption

---
+++ dist/DetectableOverflow.d.ts
@@ -0,0 +1,19 @@
+/// <reference types="react" />
+import * as React from 'react';
+export interface Props {
+    tag?: string;
+    style?: object;
+    className?: string;
+    onChange?: (isOverflowed: boolean) => void;
+}
+export interface States {
+    isOverflowed: boolean;
+}
+export declare class DetectableOverflow extends React.Component<Props, States> {
+    private ref;
+    constructor(props: Props);
+    componentDidMount(): void;
+    componentDidUpdate(): void;
+    updateState(): void;
+    render(): React.DOMElement<React.DOMAttributes<Element>, Element>;
+}
